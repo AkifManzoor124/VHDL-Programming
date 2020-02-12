77401c4b5a4109b46188c0af820aad0187c6e1e1
Akif Manzoor <akifmanzoor124@gmail.com>
2020-02-11 23:47:54 -0500

    added full adder 1 bit

---
+++ full_adder_1bit.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity full_adder is
+    port (
+        a,b,cin: in std_logic;
+        sum,cout: out std_logic;
+    );
+end full_adder;
+
+architecture func of full_adder is
+begin
+
+    sum = a XOR b XOR cin;
+    cout = (a AND B) OR (cin AND (A XOR B));
+
+end func ; -- arch
