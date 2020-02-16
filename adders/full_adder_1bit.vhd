ec7ac6298eb48d7d49b14b9e7f2d8785014cc221
Akif Manzoor <akifmanzoor124@gmail.com>
2020-02-16 05:45:00 -0500

    Updated format for full_adder_1bit in adders

--- adders/full_adder_1bit.vhd
+++ adders/full_adder_1bit.vhd
@@ -1,17 +1,20 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity full_adder_1bit is
-    port (
-        a,b,cin: in std_logic;
-        sum,cout: out std_logic
+ENTITY full_adder_1bit IS
+    PORT (
+        a : IN std_logic;
+        b : IN std_logic;
+        cin : IN std_logic;
+        sum : OUT std_logic;
+        cout : OUT std_logic
     );
-end full_adder_1bit;
+END full_adder_1bit;
 
-architecture func of full_adder_1bit is
-begin
+ARCHITECTURE func OF full_adder_1bit IS
+BEGIN
 
     sum <= a XOR b XOR cin;
     cout <= (a AND B) OR (cin AND (A XOR B));
 
-end func ; -- arch
+END func; -- arch
